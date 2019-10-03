5a5c5f33bf051b6fbe2731bdb121e6ce72d67527
KitsuneNoctus <theceintiant@gmail.com>
2019-10-02 21:35:49 -0700

    Added and successfully ran test

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from virus import Virus
 
 def test_create_population():
     virus = Virus("Small Cough",0.1,0.3)
-    group = simulation.Simulation(10,0.5,2,virus)
+    group = Simulation(10,0.5,virus,2)
     pop = group._create_population(2)
     assert len(pop) == 10
     pass
